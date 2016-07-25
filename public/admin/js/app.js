e5451d23e0df946849112292f665cac3745d1ecc
Mihaela Marinca <mihaela.marinca@evozon.com>
2016-07-25 16:24:48 +0300

    croppie avatars for the create-blog part

--- public/admin/js/app.js
+++ public/admin/js/app.js
@@ -106,3 +106,203 @@ $(document).ready(function(){
 });
 
 //----------------------------
+//Croppie avatars for the creation of the blog avatars
+
+$(document).ready(function() {
+
+// Image upload preview modal cancel button
+	$(".modal-footer .cancel-img").on('click', function(){
+		var src = $( ".blog-pic #blog_img_preview").attr('src');
+		$('#blog_img_preview').attr('src', src);
+
+		if (!$('#blog_img_preview').hasClass('defaultAvatar')) {
+			$('#blog_img_preview').addClass('hidden');
+			$('#croppie_pic').croppie('bind', {
+				url: src
+			}, function() {
+				$('#croppie_pic .cr-image').css({
+					'transform-origin': '20px 20px 0',
+					'-webkit-transform-origin': '20px 20px 0',
+					'transform': 'translate3d(20px, 20px, 0)',
+					'width': '140px',
+					'height': '140px'
+				});
+				$('#croppie_pic .cr-slider').attr('min', 1).attr('max', 2).val(1);
+			});
+
+			$('#croppie_pic').removeClass('hidden');
+		} else {
+			$('#croppie_pic').addClass('hidden');
+			$('#blog_img_preview').removeClass('hidden');
+			$('#blog_img_preview.defaultAvatar').show();
+		}
+		$('#upload_blog_img').get(0).reset();
+	});
+
+
+	$('#croppie_pic').croppie({
+		url: $('#blog_img_preview').attr('src'),
+		viewport: {
+			width: 140,
+			height: 140,
+			type: 'circle'
+		},
+		boundary: {
+			width: 180,
+			height: 180
+		}
+	});
+
+	$('#changeImgBlog').on('show.bs.modal', function() {
+		$('#croppie_pic .cr-image').css({
+			'transform-origin': '20px 20px 0',
+			'transform': 'translate3d(20px, 20px, 0)',
+			'width': 140,
+			'height': 140
+		});
+		$('#croppie_pic .cr-slider').attr('min', 1).attr('max', 2);
+	});
+
+	if (!$('#blog_img_preview').hasClass('defaultAvatar')){
+		$('#blog_img_preview').addClass('hidden');
+	} else {
+		$('#croppie_pic').addClass('hidden');
+	}
+
+	// Image upload preview
+	function readURL(input) {
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
+			reader.onload = function (e) {
+				$('#blog_img_preview').attr('src', e.target.result).addClass('hidden');
+				var imageStyle = $('#croppie_pic .cr-image').get(0).style;
+				imageStyle.removeProperty('transform-origin');
+				imageStyle.removeProperty('transform');
+				imageStyle.removeProperty('width');
+				imageStyle.removeProperty('height');
+
+				$('#croppie_pic').croppie('bind', {
+					url: e.target.result
+				}, function() {
+					var minZoom = +$('#croppie_pic .cr-image')[0].style['transform']
+						.split(")")
+						.find(function(item) {
+							return item.indexOf('scale') >=0
+						}).replace("scale(", '');
+
+					minZoom = (minZoom < 1) ? minZoom : 1;
+
+					$('#croppie_pic .cr-slider').attr('min', minZoom).attr('max', 2);
+				}).removeClass('hidden');
+			}
+			reader.readAsDataURL(input.files[0]);
+		}
+	}
+
+	function getOrientation(file, callback) {
+		var reader = new FileReader();
+		reader.onload = function(e) {
+
+			var view = new DataView(e.target.result);
+			if (view.getUint16(0, false) != 0xFFD8) return callback(-2);
+			var length = view.byteLength, offset = 2;
+
+			while (offset < length) {
+				var marker = view.getUint16(offset, false);
+				offset += 2;
+				if (marker == 0xFFE1) {
+					if (view.getUint32(offset += 2, false) != 0x45786966) return callback(-1);
+					var little = view.getUint16(offset += 6, false) == 0x4949;
+					offset += view.getUint32(offset + 4, little);
+					var tags = view.getUint16(offset, little);
+					offset += 2;
+					for (var i = 0; i < tags; i++)
+						if (view.getUint16(offset + (i * 12), little) == 0x0112)
+							return callback(view.getUint16(offset + (i * 12) + 8, little));
+				}
+				else if ((marker & 0xFF00) != 0xFF00) break;
+				else offset += view.getUint16(offset, false);
+			}
+
+			return callback(-1);
+		};
+		reader.readAsArrayBuffer(file.slice(0, 64 * 1024));
+		//	console.log(orientation);
+	}
+
+	var input = document.getElementById('file-upload-blog');
+	input.onchange = function(e) {
+		getOrientation(input.files[0], function(orientation) {
+			if (orientation === 6) {
+				$(".croppie-container .cr-boundary").rotate({animateTo:90});
+			}
+		});
+	}
+
+	$("#file-upload-blog").change(function () {
+		readURL(this);
+		//getOrientation(this);
+	});
+
+
+	//delete image
+	$(".modal-footer .delete-img").on('click', function(){
+		var themeinitial = $('#cmn-toggle-4').is(':checked');
+		$( "#file-upload-blog" ).val("");
+		if (themeinitial === false){
+			$('#blog_img_preview').attr('src', '/blog/img/blog_dark_large.png');
+		} else if (themeinitial === true) {
+			$('#blog_img_preview').attr('src', '/blog/img/blog_light_large.png');
+		}
+
+		$('[name=action_form]').val('delete');
+
+		$('#blog_img_preview').removeClass('hidden');
+		$('#croppie_pic').addClass('hidden');
+	});
+
+	// Validation file input for img only
+	function stringEndsWithValidExtension(stringToCheck, acceptableExtensionsArray, required) {
+		if (required == false && stringToCheck.length == 0) { return true; }
+		for (var i = 0; i < acceptableExtensionsArray.length; i++) {
+			if (stringToCheck.toLowerCase().endsWith(acceptableExtensionsArray[i].toLowerCase())) { return true; }
+		}
+		return false;
+	}
+
+	String.prototype.startsWith = function (str) { return (this.match("^" + str) == str) }
+	String.prototype.endsWith = function (str) { return (this.match(str + "$") == str) }
+
+	//submitting upload picture form
+	$("#changeImgBlog .save-img").click(function() {
+		if (!stringEndsWithValidExtension($("[id*='file-upload-blog']").val(), [".png", ".jpeg", ".jpg", ".bmp", ".gif"], false)) {
+			$('.error_file').fadeIn().delay(3000).fadeOut(2000);
+			return false;
+		}
+		//croppie avatars
+		var cropData= $('#croppie_pic').croppie('get');
+		var topLeftX = cropData.points[0];
+		var topLeftY = cropData.points[1];
+		var bottomRightX = cropData.points[2];
+		var bottomRightY = cropData.points[3];
+
+		var widthCrop = bottomRightX - topLeftX;
+		var heightCrop = bottomRightY - topLeftY;
+		var top = topLeftY;
+		var left = topLeftX;
+
+		$('#upload_blog_img [name=top]').val(top);
+		$('#upload_blog_img [name=left]').val(left);
+		$('#upload_blog_img [name=width]').val(widthCrop);
+		$('#upload_blog_img [name=height]').val(heightCrop);
+		$('#upload_blog_img [name=zoom]').val(cropData.zoom);
+
+
+
+		if (widthCrop !== '0' || heightCrop !== '0' || top !== '0' || left !== '0') {
+			$('[name=action_form]').val('crop');
+		}
+
+		$("#upload_blog_img").submit();
+	});
+});
