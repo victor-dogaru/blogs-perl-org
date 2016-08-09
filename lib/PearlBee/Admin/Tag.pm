d352b610fed38cab429b18ac28e2d3df9872138f
victor <victor.dogaru@evozon.com>
2016-08-09 15:47:40 +0300

    tags dashboard for superadmins

--- lib/PearlBee/Admin/Tag.pm
+++ lib/PearlBee/Admin/Tag.pm
@@ -59,26 +59,38 @@ Add a new tag
 
 post '/admin/tags/add' => sub {
 
-  my $temp_user = resultset('Users')->find_by_session(session);
-  unless ( $temp_user and $temp_user->can_do( 'create tag' ) ) {
-    warn "***** Redirecting guest away from /admin/tags/add";
-    return template 'admin/tags/list', {
-      warning => "You are not allowed to create tags",
-    }, { layout => 'admin' };
-  }
-  my @tags;
   my $name = params->{name};
+  my $nr_of_rows = 5;
   my $slug = string_to_slug( params->{slug} );
+  my @tags = resultset('Tag')->search( {}, { rows => $nr_of_rows, page => 1 } );
+  my $all  = resultset('Tag')->count; 
+  my $total_pages                 = get_total_pages($all, $nr_of_rows);
+  my ($previous_link, $next_link) = get_previous_next_link(1, $total_pages, '/author/tags');  
+  
+  my $total_posts     = $all;
+  my $posts_per_page  = $nr_of_rows;
+  my $current_page    = 1;
+  my $pages_per_set   = 5;
+  my $pagination      = generate_pagination_numbering($total_posts, $posts_per_page, $current_page, $pages_per_set);
 
   my $found_slug_or_name = resultset('Tag')->search({ -or => [ slug => $slug, name => $name ] })->first;
 
   # Check for slug or name duplicates
   if ( $found_slug_or_name ) {
-    @tags = resultset('Tag')->all;
-
-    template 'admin/tags/list', { warning => "The tag name or slug already exists", tags => \@tags } , { layout => 'admin' };
-  }
+  template 'admin/tags/list',
+    {
+     warning       => 'The tag name or slug already exists',
+     all           => $all, 
+     page          => 1,
+     next_link     => $next_link,
+     previous_link => $previous_link,
+     action_url    => 'author/tags/page',
+     pages         => $pagination->pages_in_set,
+     tags          => \@tags 
+    }, 
+    { layout => 'admin' };  }
   else {
+
     try {
       my $tag = resultset('Tag')->create({
         name   => $name,
@@ -89,12 +101,18 @@ post '/admin/tags/add' => sub {
       info "Could not create tag named '$name'";
     };
 
-    @tags = resultset('Tag')->all;
-
-    template 'admin/tags/list', {
-      success => "The cateogry was successfully added.",
-      tags => \@tags
-    }, { layout => 'admin' };
+    template 'admin/tags/list',
+    {
+     success       => 'The tag was added successfully',
+     all           => $all, 
+     page          => 1,
+     next_link     => $next_link,
+     previous_link => $previous_link,
+     action_url    => 'admin/tags/page',
+     pages         => $pagination->pages_in_set,
+     tags          => \@tags 
+    }, 
+    { layout => 'admin' };
   }
 
 };
@@ -107,16 +125,17 @@ Delete method
 
 get '/admin/tags/delete/:id' => sub {
 
-  my $tag_id   = params->{id};
+my $tag_id   = params->{id};
   my $tag      = resultset('Tag')->find( $tag_id );
   my $res_user = resultset('Users')->find_by_session(session);
   unless ( $res_user and $res_user->can_do( 'delete tag' ) ) {
-    warn "***** Redirecting guest away from /admin/tags/delete/:id";
+    warn "***** Redirecting guest away from /author/tags/delete/:id";
     info "You are not allowed to delete tags, please create an account";
-    redirect '/admin/tags';
+    redirect '/author/tags';
   }
 
   # Delete first all many to many dependecies for safly removal of the isolated tag
+
   try {
     foreach ( $tag->post_tags ) {
       $_->delete;
@@ -141,23 +160,24 @@ edit method
 
 any '/admin/tags/edit/:id' => sub {
 
+
   my $tag_id = params->{id};
-  my @tags   = resultset('Tag')->all;
+  my $user   = resultset('Users')->find_by_session(session);
+  my $nr_of_rows = 5;
   my $tag    = resultset('Tag')->find( $tag_id );
-  my $res_user         = resultset('Users')->find_by_session(session);
-  unless ( $res_user and $res_user->can_do( 'update tag' ) ) {
-    warn "***** Redirecting guest away from /admin/tags/edit/:id";
-    template 'admin/tags/list',
-      {
-      message => "You are not allowed to update tags, please create an account",
-      tag  => $tag,
-      tags => \@tags
-      },
-      { layout => 'admin' };
-  }
 
   my $name = params->{name};
   my $slug = string_to_slug( params->{slug} );
+  my @tags = resultset('Tag')->search( {}, { rows => $nr_of_rows, page => 1 } );
+  my $all  = resultset('Tag')->count;
+  my $total_pages                 = get_total_pages($all, $nr_of_rows);
+  my ($previous_link, $next_link) = get_previous_next_link(1, $total_pages, '/author/tags');
+  
+  my $total_posts     = $all;
+  my $posts_per_page  = $nr_of_rows;
+  my $current_page    = 1;
+  my $pages_per_set   = 5;
+  my $pagination      = generate_pagination_numbering($total_posts, $posts_per_page, $current_page, $pages_per_set);
 
   # Check if the form was submited
   if ( $name && $slug ) {
@@ -167,26 +187,35 @@ any '/admin/tags/edit/:id' => sub {
     # Check if the user entered an existing slug
     if ( $found_slug ) {
 
-      template 'admin/tags/list',
-        {
-        tag     => $tag,
-        tags    => \@tags,
-        warning => 'The tag slug already exists'
-        },
-        { layout => 'admin' };
+    template 'admin/tags/list',
+    {
+       warning       => 'The slug already exists',
+       all           => $all, 
+       page          => 1,
+       next_link     => $next_link,
+       previous_link => $previous_link,
+       action_url    => 'admin/tags/page',
+       pages         => $pagination->pages_in_set,
+       tags          => \@tags 
+    }, 
+    { layout => 'admin' };
 
     }
     # Check if the user entered an existing name
     elsif ( $found_name ) {
 
-      template 'admin/tags/list',
-        {
-        tag     => $tag,
-        tags    => \@tags,
-        warning => 'The tag name already exists'
-        },
-      { layout => 'admin' };
-
+    template 'admin/tags/list',
+    {
+       warning       => 'The name already exists',
+       all           => $all, 
+       page          => 1,
+       next_link     => $next_link,
+       previous_link => $previous_link,
+       action_url    => 'admin/tags/page',
+       pages         => $pagination->pages_in_set,
+       tags          => \@tags 
+    }, 
+    { layout => 'admin' };
     }
     else {
       try {
@@ -200,15 +229,22 @@ any '/admin/tags/edit/:id' => sub {
         error "Could not update tag named '$name'";
       };
 
-      @tags = resultset('Tag')->all;
+      my @tags = resultset('Tag')->search( {}, { rows => $nr_of_rows, page => 1 } );
+      my $all  = resultset('Tag')->count;
 
       template 'admin/tags/list',
-        {
-        tag     => $tag,
-        tags    => \@tags,
-        success => 'The tag was updated successfully'
-        },
+      {
+         success       => 'The slug was updated successfully',
+         all           => $all, 
+         page          => 1,
+         next_link     => $next_link,
+         previous_link => $previous_link,
+         action_url    => 'admin/tags/page',
+         pages         => $pagination->pages_in_set,
+         tags          => \@tags 
+      }, 
       { layout => 'admin' };
+    
     }
   }
   else {
