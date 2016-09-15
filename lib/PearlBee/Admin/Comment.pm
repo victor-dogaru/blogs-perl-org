fe86436278838934414f7178e7b0196c37db350f
victor <victor.dogaru@evozon.com>
2016-09-15 16:52:43 +0300

    BLOGSPERL-599 Next/back buttons form the comments table doesn't work

--- lib/PearlBee/Admin/Comment.pm
+++ lib/PearlBee/Admin/Comment.pm
@@ -216,28 +216,75 @@ get '/admin/comments/blog/:blog/:status/page/:page' => sub {
   my $page       = params->{page} || 1;
   my $blog       = params->{blog};
   my $status     = params->{status};
-  my $blog_ref   = resultset('Blog')->find({name => params->{blog}});
-  my $user       = resultset('Users')->find_by_session(session);
-  my @blog_posts = resultset('BlogPost')->search({ blog_id => $blog_ref->get_column('id')});
+  my $blog_ref;
+  my @blog_posts;
   my @comments;
-  my @blogs      = resultset('Blog')->all(); 
+  my $user       = resultset('Users')->find_by_session(session);
+  my @blogs      = resultset('Blog')->all();
+
+  if (params->{blog} ne 'all'){
+     $blog_ref   =resultset('Blog')->find({name => params->{blog}});
+     @blog_posts = resultset('BlogPost')->search({ blog_id => $blog_ref->get_column('id')});
+  }
+  else
+  {
+    foreach my $iterator (@blogs){
+      push @blog_posts, resultset('BlogPost')->search({ blog_id => $iterator->get_column('id')});
+    }
+  }
+
+  my @posts; 
+  my @ids;
+
+  foreach my $i (@blog_posts){
+    push @posts, resultset ('Post')->search({ id => $i->post_id})
+  };
+  
+  foreach my $i (@posts){
+     if ($i->user_id == $user->id){
+      push @ids, $i->id;
+     }
+  }
+
   if ($status eq ('all') ){
     foreach my $blog_post (@blog_posts){
-      push @comments, map { $_->as_hashref }
+      push @comments, 
               resultset('Comment')->search({post_id => $blog_post->post_id});
     }
   }
   else{
     foreach my $blog_post (@blog_posts){
-      push @comments, map { $_->as_hashref }
+      push @comments, 
               resultset('Comment')->search({post_id => $blog_post->post_id, status => $status});
     }
   }
-  my $all        = scalar @comments;
+
+
+  my $all       = scalar @comments;
+  my $approved  = 0;
+  my $spam      = 0;
+  my $trash     = 0;
+  my $pending   = 0;
+
+  foreach my $iterator(@comments){
+    if ($iterator->status eq 'approved')
+    {
+      $approved ++;
+    }
+    elsif ($iterator->status eq 'pending'){
+      $pending++;
+    }
+    elsif ($iterator->status eq 'trash'){
+      $trash++;
+    }
+    else{
+      $spam++;
+    }
+  }
 
   # Calculate the next and previous page link
   my $total_pages                 = get_total_pages($all, $nr_of_rows);
-  my ($previous_link, $next_link) = get_previous_next_link($page, $total_pages, '/admin/comments/' . $blog);
+  my ($previous_link, $next_link) = get_previous_next_link($page, $total_pages, '/admin/comments/blog/' . $blog . '/'. $status);
 
   # Generating the pagination navigation
   my $total_comments  = $all;
@@ -245,31 +292,17 @@ get '/admin/comments/blog/:blog/:status/page/:page' => sub {
   my $current_page    = $page;
   my $pages_per_set   = 7;
   my $pagination      = generate_pagination_numbering($total_comments, $posts_per_page, $current_page, $pages_per_set);
-  
-  my $pending  = 0;
-  my $trash    = 0; 
-  my $spam     = 0;
-  my $approved = 0;
-  foreach my $comment (@comments){
-    if ($comment->{status} eq 'approved'){
-      $approved +=1;
-    }
-    elsif ($comment->{status} eq 'pending'){
-      $pending +=1;
-    }
-    elsif ($comment->{status} eq 'spam'){
-      $spam +=1;
-    }
-    else{
-      $trash +=1;
-    }
-  }
 
-  my @actual_comments = splice(@comments,($page-1)*$nr_of_rows,$nr_of_rows);
+
+  my @sorted_comments = sort {$b->id <=> $a->id} @comments;
+  my @actual_comments = splice(@sorted_comments,($page-1)*$nr_of_rows,$nr_of_rows);
+  map { $_->as_hashref } @actual_comments;
+  
   template 'admin/comments/list',
       {
         comments      => \@actual_comments,
         blogs         => \@blogs,
+        searched_blog => $blog_ref,
         all           => $all,
         approved      => $approved,
         spam          => $spam,
@@ -278,7 +311,7 @@ get '/admin/comments/blog/:blog/:status/page/:page' => sub {
         page          => $page,
         next_link     => $next_link,
         previous_link => $previous_link,
-        action_url    => 'admin/comments/blog/' . $blog . '/'. $status .'/page',
+        action_url    => 'admin/comments/blog/' . $blog . '/' . $status . '/page',
         pages         => $pagination->pages_in_set
       },
       { layout => 'admin' };
